8aff2e460f98eb2f5da443f796ab8b205856e9c4
Thushanthadasun <thushanthadasun25@gmail.com>
2025-08-29 05:59:15 +0530

    Update db.mjs

--- db.mjs
+++ db.mjs
@@ -1,11 +1,14 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new pg.Pool({
-  user: 'postgres',
-  host: 'vscdatabase.cxaogewmsid6.ap-south-1.rds.amazonaws.com',
-  database: 'service-center',
-  password: 'Vehicle12345',
-  port: 5432,
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'vscdatabase.cxaogewmsid6.ap-south-1.rds.amazonaws.com',
+  database: process.env.DB_NAME || 'service-center',
+  password: process.env.DB_PASSWORD || 'Vehicle12345',
+  port: process.env.DB_PORT || 5432,
   ssl: {
     rejectUnauthorized: false, // Required for AWS RDS or any SSL-based connection
   },
